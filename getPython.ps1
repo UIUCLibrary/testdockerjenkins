403826a3b93ed6ac60ad1b68474dcda6b8ff8a27
Henry Borchers <henryborchers@yahoo.com>
2018-03-07 12:18:40 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -1,4 +1,6 @@
 $ErrorActionPreference = "Stop"
+Write-Host "Pinging Google"
+ping www.google.com
 
 Write-Host "Downloading python"
 Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe' -UseBasicParsing -OutFile 'python-3.6.4-amd64.exe'
