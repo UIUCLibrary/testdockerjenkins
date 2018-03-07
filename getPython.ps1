481c6956f2ef2bd9ddec5822995c0be853421487
Henry Borchers <henryborchers@yahoo.com>
2018-03-07 12:13:50 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -1,4 +1,6 @@
 $ErrorActionPreference = "Stop"
+Write-Host "Downloading something docker"
+Invoke-WebRequest "https://master.dockerproject.org/windows/x86_64/docker.zip" -OutFile "docker.zip" -UseBasicParsing
 Write-Host "Downloading python"
 Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe' -UseBasicParsing -OutFile 'python-3.6.4-amd64.exe'
 dir
