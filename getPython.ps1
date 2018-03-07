97c578e6feda0ddfbb7627185d19f15b2c5cd94a
Henry Borchers <henryborchers@yahoo.com>
2018-03-07 12:03:25 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop"
 Write-Host "Downloading python"
 Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe' -OutFile 'python-3.6.4-amd64.exe'
 dir
