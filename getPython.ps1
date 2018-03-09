c81562d5727e66bac0644559db2b5f03c0433ff4
Henry Borchers <henryborchers@yahoo.com>
2018-03-09 11:25:48 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -5,7 +5,7 @@ Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd
 Invoke-Expression -Command "dir"
 
 Write-Host "Installing Python"
-Invoke-Expression -Command '.\python-3.6.4-amd64.exe /quiet InstallAllUser=0  PrependPath=1 Include_test=0 SimpleInstall=1 SimpleInstallDescription=\"Just for me, no test suite.\"'
+Start-Process -FilePath .\python-3.6.4-amd64.exe -ArgumentList ("/quiet", "InstallAllUser=1 PrependPath=1 Include_test=0 TargetDir=c:\Python") -Wait
 Write-Host "Python is installed"
 Get-ChildItem -Recurse c:\Users
 
