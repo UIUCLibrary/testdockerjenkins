af01b82dc5d76c7f2ea8b23dba98e0df612931fc
Henry Borchers <henryborchers@yahoo.com>
2018-03-09 10:11:26 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = "Stop"
 
 Write-Host "Downloading python"
 Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe' -UseBasicParsing -OutFile 'python-3.6.4-amd64.exe'
+Invoke-Expression -Command "dir"
 Write-Host "Installing python"
 Invoke-Expression -Command '.\python-3.6.4-amd64.exe --help'
 Invoke-Expression -Command '.\python-3.6.4-amd64.exe /passive InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstall=1 SimpleInstallDescription=\"Just for me, no test suite.\"'
