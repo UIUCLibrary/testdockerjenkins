77747be8621e91d1bb27ada262392b5536ccc0e1
Henry Borchers <henryborchers@yahoo.com>
2018-03-09 09:57:40 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -9,6 +9,6 @@ ping www.google.com
 Write-Host "Downloading python"
 Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe' -UseBasicParsing -OutFile 'python-3.6.4-amd64.exe'
 Write-Host "Installing python"
-.\python-3.6.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+.\python-3.6.4-amd64.exe /passive InstallAllUsers=1 PrependPath=1 Include_test=0
 Write-Host "Python is installed"
 Invoke-Expression -Command "py --version"
