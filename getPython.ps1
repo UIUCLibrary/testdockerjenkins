4b419144caadf1d454f8d8c2dd473cc470caceca
Henry Borchers <henryborchers@yahoo.com>
2018-03-09 10:48:00 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -7,7 +7,7 @@ Invoke-Expression -Command "dir"
 Write-Host "Installing Python"
 Invoke-Expression -Command '.\python-3.6.4-amd64.exe /quiet InstallAllUser=0  PrependPath=1 Include_test=0 SimpleInstall=1 SimpleInstallDescription=\"Just for me, no test suite.\"'
 Write-Host "Python is installed"
-Get-ChildItem -Recurse c:\
+Get-ChildItem -Recurse c:\Users
 
 Get-Command python
 # Invoke-Expression -Command "py --version"
