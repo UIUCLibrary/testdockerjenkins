061fd087170671062c7df064bf8965c10f07fa3b
Henry Borchers <henryborchers@yahoo.com>
2018-03-09 10:02:40 -0600

    Update getPython.ps1

--- getPython.ps1
+++ getPython.ps1
@@ -1,11 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "Checking network"
-ipconfig
-
-Write-Host "Pinging Google"
-ping www.google.com
-
 Write-Host "Downloading python"
 Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe' -UseBasicParsing -OutFile 'python-3.6.4-amd64.exe'
 Write-Host "Installing python"
